ccc3da093c9004f67e3117fdedd5581b46bea8ec
Sunatqayoom <115891537+Sunatqayoom@users.noreply.github.com>
2024-12-28 19:52:23 +0530

    greedy

---
+++ src/Greedy/Assigncookies.java
@@ -0,0 +1,29 @@
+package Greedy;
+
+import java.util.Arrays;
+
+public class Assigncookies {
+    public static void main(String[] args) {
+        int g[] =  {1,2,3};
+        int s[] = {1,1};
+        System.out.println(findContentChildren(g,s));
+    }
+
+    public static int findContentChildren(int[] g, int[] s) {
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int left=0;
+        int right=0;
+
+        while(left< g.length && right < s.length) {
+            if(g[left] <= s[right]) {
+                left++;
+            }
+            right++;
+        }
+        return left;
+
+    }
+}
